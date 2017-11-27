70130f40e9d90b300c4b11e7dbce411fd26d4ebe
Trimendos <trimendos@gmail.com>
2017-11-27 11:28:23 +0200

    prototype ver.: 0.1

---
+++ dev/js/league_table.js
@@ -0,0 +1,108 @@
+document.addEventListener("DOMContentLoaded", function(){
+    "use strict";
+
+    const options = document.getElementsByTagName("option");
+
+    for (let i=0; i < options.length; i++) {
+        const childElement = options[i];
+        childElement.addEventListener("input", function (e) {
+            console.log("option input", e);
+        })
+    }
+
+    function makeAutoComplete(data) {
+        const leagueNamesList = data.map(el => el.caption);
+        let leagueObj = {};
+        for(let i in data) {
+            leagueObj[data[i].caption] = data[i].id;
+        }
+
+        const dataList = document.getElementById("search_field");
+        for(let i in leagueNamesList){
+            const option = document.createElement("option");
+            option.text = leagueNamesList[i];
+            dataList.appendChild(option);
+        }
+        console.log(leagueObj);
+        return leagueObj;
+    }
+
+    function clearTable() {
+        const tb = document.getElementsByTagName("tbody");
+        console.log("tb:", tb, typeof tb);
+        for(let i=0; i < tb.length; i++) {
+            tb[i].innerHTML = "";
+        }
+    }
+    
+    function fillTable(data) {
+        console.log("fillTable[data]", data);
+        console.log("fillTable type[data]", typeof data);
+
+        if ('content' in document.createElement('template')) {
+
+            const t = document.querySelector("#league_row"),
+                td = t.content.querySelectorAll("td"),
+                caption = document.getElementById("caption_row"),
+                captionTds = caption.content.querySelectorAll("td");
+            captionTds[0].textContent = data.leagueCaption;
+            captionTds[1].textContent = data.matchday;
+            const captionTb = document.getElementsByTagName("tbody");
+            const clone = document.importNode(caption.content, true);
+            captionTb[0].appendChild(clone);
+
+            data.standing.forEach((el) => {
+                td[0].firstChild.setAttribute("src", el.crestURI !== null ? el.crestURI : "./img/default-icon.svg");
+                td[1].textContent = el.teamName;
+                td[2].textContent = el.position;
+                td[3].textContent = el.points;
+                td[4].textContent = el.playedGames;
+                td[5].textContent = el.wins;
+                td[6].textContent = el.losses;
+                td[7].textContent = el.draws;
+                td[8].textContent = el.goals;
+                td[9].textContent = el.goalsAgainst;
+                td[10].textContent = el.goalDifference;
+                const tb = document.getElementsByTagName("tbody");
+                const clone = document.importNode(t.content, true);
+                tb[1].appendChild(clone);
+            });
+
+        } else {
+
+        }
+    }
+
+
+    const url = "https://api.football-data.org/v1/competitions/?season=2017";
+    const headers = {"X-Auth-Token": "71d6556f803d4391bf1bede6e10cf5d8"};
+    console.log("competitions:");
+    fetch(url, {"headers": headers})
+        .then((resp) =>
+            resp.json()
+        )
+        .then(data => {
+                console.log("data", data);
+                const list = makeAutoComplete(data);
+            const searchField = document.querySelector(".search-field");
+
+            searchField.addEventListener("input", () => {
+                if(list.hasOwnProperty(searchField.value)) {
+                    console.log(list[searchField.value]);
+                    console.log("leagueTable:");
+                    const url = "http://api.football-data.org/v1/competitions/" + list[searchField.value] + "/leagueTable";
+                    const headers = {"X-Auth-Token": "71d6556f803d4391bf1bede6e10cf5d8"};
+                    fetch(url, {"headers": headers})
+                        .then((resp) => resp.json())
+                        .then(data => {
+                            console.log("data", data);
+                            clearTable();
+                            fillTable(data);
+                        })
+                        .catch(error => console.log("error", error));
+                }
+
+            });
+        })
+        .catch(error => console.log("error", error));
+});
---
+++ dev/js/team.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", function(){
+    "use strict";
+
+    const form = document.querySelector("#search-form");
+    form.addEventListener("submit", function (e) {
+        const selector = "a[name='" + form.querySelector("input").value +"']";
+        document.querySelector(selector).scrollIntoView();
+        console.log(selector);
+        console.log(e);
+        e.preventDefault();
+    }, false);
+
+    function makeAutoComplete(teamList) {
+        const dataList = document.getElementById("search_field");
+        for(let i in teamList){
+            const option = document.createElement("option");
+            option.text = teamList[i];
+            dataList.appendChild(option);
+        }
+    }
+
+    function clearTable() {
+        const tb = document.getElementsByTagName("tbody");
+        console.log("tb:", tb, typeof tb);
+        for(let i=0; i < tb.length; i++) {
+            tb[i].innerHTML = "";
+        }
+    }
+
+    function replaceProtocol(url){
+        return url.replace("http:", "https:")
+    }
+
+    function fillNullData(str) {
+        return str !== null ? str : " - ";
+    }
+
+    function fillTable(data) {
+        console.log("fillTable[teams]", data);
+        if ('content' in document.createElement('template')) {
+            // Instantiate the table with the existing HTML tbody and the row with the template
+            const t = document.querySelector("#teams_row"),
+                td = t.content.querySelectorAll("td");
+
+            data.forEach((el) => {
+                td[0].firstChild.setAttribute("src",
+                    el.crestUrl !== null ? replaceProtocol(el.crestUrl) : "./img/default-icon.svg");
+                td[0].lastChild.setAttribute("name", el.name);
+                td[1].textContent = fillNullData(el.code);
+                td[2].textContent = fillNullData(el.name);
+                td[3].textContent = fillNullData(el.shortName);
+
+                const tb = document.getElementsByTagName("tbody");
+                const clone = document.importNode(t.content, true);
+                tb[0].appendChild(clone);
+            });
+
+        } else {
+
+        }
+    }
+
+    const url = "https://api.football-data.org/v1/competitions/";
+    const headers = {"X-Auth-Token": "71d6556f803d4391bf1bede6e10cf5d8"};
+    console.log("competitions:");
+    fetch(url, {"headers": headers})
+        .then((resp) =>
+            resp.json()
+        )
+        .then(data => {
+            console.log("data", data);
+            const headers = {"X-Auth-Token": "71d6556f803d4391bf1bede6e10cf5d8"};
+            const competitionIds = data.map(el => el.id);
+            const urls = competitionIds.map(
+                el => "https://api.football-data.org/v1/competitions/id/teams".replace("id", el));
+            console.log("competitionIds", competitionIds);
+            console.log("urls", urls);
+            Promise.all(urls.map(url => fetch(url, {"headers": headers})))
+                .then(response => Promise.all(response.map(resp => resp.json())))
+                .then(data => {
+                    console.log("Teams data", data);
+                    let teams = [];
+                    let teamsNameList = [];
+                    data.forEach(el => {
+                        if( el.teams !== undefined) {
+                            for(let i=0; i < el.teams.length; i++){
+                                teams.push(el.teams[i]);
+                                teamsNameList.push(el.teams[i].name);
+                            }
+                        }
+                    });
+                    console.log("teams", teams);
+                    console.log("teamsNameList", teamsNameList);
+                    teamsNameList = teamsNameList.filter(function(item, pos) {
+                        return teamsNameList.indexOf(item) === pos;
+                    });
+                    console.log("teamsNameList", teamsNameList);
+                    makeAutoComplete(teamsNameList);
+                    fillTable(teams);
+                })
+                .catch(error => console.log("error", error));
+        })
+        .catch(error => console.log("error", error));
+});
